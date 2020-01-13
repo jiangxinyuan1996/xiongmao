2c970671f2a3ff52ac1944153fd0ac36979f00e5
gan <554695146@qq.com>
2020-01-13 18:36:31 +0800

    gan first

---
+++ src/Components/DetailSwiper/detailswiper.js
@@ -0,0 +1,25 @@
+
+import React ,{Component} from 'react'
+import Swiper from 'swiper'
+import "swiper/css/swiper.min.css"
+class MySwiper extends Component{
+    render(){
+        return <div className="swiper-container Gan">
+            <div className="swiper-wrapper">
+                {this.props.children}
+            </div>
+            <div className="swiper-pagination"></div>
+        </div>
+    }
+    componentDidMount() {
+        new Swiper('.Gan',{
+            pagination: {
+                el: '.swiper-pagination',
+                bulletElement : 'li',
+              },
+            autoplay:true,
+        })
+    }
+    
+}
+export default MySwiper
--- src/Router/index.js
+++ src/Router/index.js
@@ -7,18 +7,26 @@ import {
 } from 'react-router-dom'
 import App from '../App'
 import Home from '../Views/Home/home'
+// import Detail from '../Views/Detail/detail'
+// import Bige from '../Views/Bige/bige'
 
 
 
 const router = <HashRouter>
     <App>
         <Switch>
-        <Route path="/" render={()=>
-            <Home>
-            </Home>
-        }></Route>
-        <Redirect from="/" to="/" exact></Redirect>
-        <Redirect to="/error"></Redirect>
+            <Route path="/home" render={() =>
+                <Home>
+                </Home>
+            } ></Route>
+
+
+            {/* <Route path="/column/:myid" component={Detail} exact/>
+        <Route path="/column" component={Bige}/> */}
+
+           
+            <Redirect from="/" to="/home" exact></Redirect>
+            <Redirect to="/error"></Redirect>
         </Switch>
     </App>
 </HashRouter>
